88f69afaaa8669e6e4713caf9c1d45f799a8ab7c
LordNocturnus <41686329+LordNocturnus@users.noreply.github.com>
2024-09-14 15:21:52 +0200

    added rng to customer for "deterministic" randomness

--- src/customer.py
+++ src/customer.py
@@ -1,14 +1,16 @@
-
+import numpy.random as npr
 
 class Customer:
 
-    def __init__(self, env, shopping_list: dict[str, int], basket: bool, path: str, start_time: float, search_bounds: (float, float), ucid: int):
+    def __init__(self, env, shopping_list: dict[str, int], basket: bool, route: str, start_time: float,
+                 search_bounds: (float, float), ucid: int, seed: int):
         self.env = env
         self.shopping_list = shopping_list
         self.basket = basket
-        self.path = path
+        self.route = route
         self.start_time = start_time
-        self.ucid = ucid
+        self.ucid = ucid # unique customer id
         self.search_bounds = search_bounds
+        self.rng = npr.default_rng(seed)
 
         print(f"{ucid} entered at {start_time}")
