79d79da6e99b1f4086180588b456617a5ad204d1
jakub <j.d.siemaszko@student.tudelft.nl>
2024-09-26 12:22:48 +0200

    sim animation done-ish

--- src/simAnimation.py
+++ src/simAnimation.py
@@ -1,20 +1,22 @@
 import pygame
-from matplotlib import pyplot as plt
+import matplotlib.pyplot as plt
 from datetime import timedelta
 import numpy as np
 
 
+
 class simAnimation():
-    def __init__(self, sim, run_index=0, window_size=(800, 600), time_scale_anim=1/3600, timestep_sim_seconds = 1):
+    def __init__(self, sim, run_index=0, window_size=(800, 600), fps = 24, timestep_sim_seconds = 1):
         self.sim = sim
-        self.resources = sim.resourceLog[run_index]
-        self.customers = sim.customerLog[run_index]
-        self.departments = sim.departmentLog[run_index]
+        self._run_index = run_index
+        self.update_resources()
+        self.max_runs = len(self.sim.resourceLog)
 
         # initialize pygame elements
         pygame.init()
         self.window_size = window_size
         self.window = pygame.display.set_mode(window_size)
+        self.surface = pygame.Surface(window_size, pygame.SRCALPHA)
 
         # window margin
         self.margin = 5 # window units
@@ -23,56 +25,93 @@ class simAnimation():
         self.sim_time = 0 # initialize sim time at zero
         self.sim_timestep = timestep_sim_seconds
         self.sim_end_time = self.customers.simulation_end_time
-        self.time_scale_anim = time_scale_anim  # ratio of animation time to simulation time ( default: 1 second / 1 hour)
+
+        self.fps = fps
+
+        # self.time_scale_anim = (1 / fps) / timestep_sim_seconds # ratio of animation time to simulation time ( default: 1 second / 1 hour)
          # tick steptime for pygame, in miliseconds
-        self.time_step_anim_miliseconds = int(timestep_sim_seconds * self.time_scale_anim * 1000)
+        # self.time_step_anim_miliseconds = int(timestep_sim_seconds * self.time_scale_anim * 1000)
 
         self.timefont = pygame.font.Font('freesansbold.ttf', 32)
         self.departmentfont = pygame.font.Font('freesansbold.ttf', 16)
-        self.availabilityfont = pygame.font.Font('freesansbold.ttf', 12)
-
-
-
+        self.availabilityfont = pygame.font.Font('freesansbold.ttf', 16)
+        self.runinfofont = pygame.font.Font('freesansbold.ttf', 32)
 
         self.colors = {
             'white' : (255, 255, 255),
-            'black': (0, 0, 0),
-            'green' : (0, 255, 0),
-            'blue' : (0, 0, 255),
-            'red' : (255, 0, 0)
+            'black': (0, 0, 0, 255),
+            'green' : (0, 255, 0, 255),
+            'blue' : (0, 0, 255, 255),
+            'red' : (255, 0, 0, 255)
         }
 
+        departments = self.departments.keys()
+        cmap = plt.cm.get_cmap('gist_rainbow', len(departments))
+        self.department_colors = {dept: tuple([int(c * 255) for c in cmap(i)]) for i, dept in enumerate(departments)}
+
+        self.bar_color_positive = self.colors['green']
+        self.bar_color_negative = self.colors['red']
+
+        self.rectangle_radius = 20
+
         self.department_ybox = 0
+        self.timer_ybox = 0
+
+        self.looped = False # flag set to true if all runs were displayed
+
+    def update_resources(self): # update resources to the current run
+        self.resources = self.sim.resourceLog[self.run_index]
+        self.customers = self.sim.customerLog[self.run_index]
+        self.departments = self.sim.departmentLog[self.run_index]
+
+    @property
+    def run_index(self):
+        return self._run_index
+
+    @run_index.setter
+    def run_index(self, other):
+        self._run_index = other
+        self.update_resources()
+
 
     def run(self, loopTillQuit = True):
-        while self.sim_time < self.sim_end_time or loopTillQuit: # run until end time is reached or loop until quit
-            self.window.fill(self.colors['white'])  # set background color
+        clock = pygame.time.Clock()
+        while not self.looped or loopTillQuit: # run until end time is reached or loop until quit
+            self.surface.fill(self.colors['white'])  # reset surface
             for event in pygame.event.get():
                 if event.type == pygame.QUIT: # quit animation if requested
                     pygame.quit()
                     quit()
 
-            #TODO: ADD STUFF TO WINDOW
-            #0) simulation time display
+            # display run info in top-right corner
+            if self.max_runs > 1:
+                self.display_run_info()
+
+            # display simulation time in top-left corner
             self.display_timer() # timer in top-right corner
 
-            #1) departments with text: name of dept, no. of customers inside
-            #2) for dept with queues, add queue length(s)
+            # display department and resource bars
             self.display_departments()
             self.display_availability_bars()
 
-            #3) entrance and checkout boxes, with queue lengths and shopping cart/basket availability
-            #4) (optional) running event log
-            #5) (very optional) customer movement between departments
+            # NOT IMPLEMENTED:
+            # 1) running resource text log
+            # 2) customer movement animation
 
             # end-of-loop routine
-            pygame.time.delay(self.time_step_anim_miliseconds) # iterate animation time
             self.sim_time += self.sim_timestep
 
-            # reset sim timer once end time reached
+            # reset sim timer once end time reached, go to next run if any
             if self.sim_time > self.sim_end_time:
                 self.sim_time = 0.0
+                if self.run_index == self.max_runs - 1:
+                    self.looped = True # flag loop over all runs
+                self.run_index = (self.run_index + 1) % self.max_runs # go to the next run
+
+            self.window.blit(self.surface, dest=(0, 0))
             pygame.display.update() #update the window view at the end of the loop
+
+            clock.tick(self.fps)
 
     def display_timer(self):
         # TIME WINDOW
@@ -80,7 +119,9 @@ class simAnimation():
                                     True,
                                     self.colors['black'],
                                     self.colors['white'])
-        self.window.blit(timetext, (self.margin, self.margin))  # draw text at top-right corner
+        self.surface.blit(timetext, (self.margin, self.margin))  # draw text at top-right corner
+        _, htext = timetext.get_size()
+        self.timer_ybox = self.margin + htext
 
     def simulation_time_text(self):
         sec = 8 * 3600 + self.sim_time # START AT 8 AM => add 8 hours to timer
@@ -88,23 +129,25 @@ class simAnimation():
         return str(td)
 
     def display_departments(self):
-        NDEPS = len(self.departments)
 
         rect_height = self.window_size[1] / 20
 
         xrect = 0 + self.margin
-        yrect = rect_height + self.margin
+        yrect = self.timer_ybox + self.margin + self.window_size[1] / 20
 
         for key, dep in self.departments.items():
 
             rect_width = self.department_rect_width(dep)
             rect = pygame.Rect((xrect, yrect), (rect_width, rect_height))
-            _ = pygame.draw.rect(self.window, self.colors['blue'], rect)
+            _ = pygame.draw.rect(self.surface, self.department_colors[key], rect, border_radius=self.rectangle_radius)
 
             depttext = self.departmentfont.render(self.department_text(dep),
                                             True,
                                             self.colors['black'],)
-            self.window.blit(depttext, (xrect + self.margin, yrect+rect_height/2))
+
+            _, htext = depttext.get_size()
+            shift = (rect_height - htext) / 2
+            self.surface.blit(depttext, (xrect + 4 * self.margin, yrect + shift))
 
             yrect += rect_height + self.margin
 
@@ -113,7 +156,7 @@ class simAnimation():
 
     def department_rect_width(self, dep):
         no_customers = self.get_customers_in_dep(dep)
-        base_rect_width = self.window_size[0] / 3 # arbitrary
+        base_rect_width = self.window_size[0] / 2 # arbitrary
         size_range = self.window_size[0] - base_rect_width - 2 * self.margin
 
         return base_rect_width + size_range * (1 - np.exp(-no_customers * 0.1386)) # arbitrary interpolation function
@@ -147,19 +190,13 @@ class simAnimation():
     def display_availability_bars(self):
 
         xmid = self.window_size[0] * 1/2
-        ymid = self.department_ybox + self.margin
-        height = self.window_size[1] / 20
+        ymid = self.department_ybox + self.margin + self.window_size[1] / 20
+        height = self.window_size[1] / 15
         for _, resource in self.resources.items():
-            if isinstance(resource,
-                          list): # checkouts!
-                continue
+            if not isinstance(resource,list) and resource.name == "Baskets": # checkouts
+                    continue
+
             self.draw_availability_bar(resource, (xmid, ymid), total_width=self.window_size[0] - 2 * self.margin, total_height=height)
-
-            ymid += height + self.margin
-
-        for r in self.resources['checkout']:
-            self.draw_availability_bar(r, (xmid, ymid),
-                                       total_width=self.window_size[0] - 2 * self.margin, total_height=height)
 
             ymid += height + self.margin
 
@@ -174,27 +211,44 @@ class simAnimation():
         if total_height is None:
             total_height = self.window_size[1]/10 # arbitrary
 
+        if not isinstance(resource, list):  # checkouts!
+            current_availability = resource.capacity - self.get_queue_length(resource)
+            rect_width = total_width / 2 * (1 - np.exp(- abs(current_availability) / resource.capacity * 5)) # arbitrary
+            resource_name = resource.name
+        else:
+            total_capacity = sum(r.capacity for r in resource)
+            total_queue = sum(self.get_queue_length(r) for r in resource)
+            current_availability = total_capacity - total_queue
+            rect_width = total_width / 2 * (
+                        1 - np.exp(- abs(current_availability) / total_capacity * 5))  # arbitrary
+            resource_name = "Checkouts" # AAAAAAAAAAAA
 
-        current_availability = resource.capacity - self.get_queue_length(resource)
 
-        rect_width = total_width / 2 * (1 - np.exp(- abs(current_availability) / resource.capacity * 5)) # arbitrary
         xrect = midpoint[0] if current_availability > 0 else midpoint[0] - rect_width
         yrect = midpoint[1]
 
-        availtext = self.availabilityfont.render('{} availability: {}'.format(resource.name, current_availability),
+        availtext = self.availabilityfont.render('{} availability: {}'.format(resource_name, current_availability),
                                               True,
                                               self.colors['black'], )
 
-        self.window.blit(availtext, (midpoint[0] + self.margin, yrect))
+        self.surface.blit(availtext, (midpoint[0] + self.margin, yrect))
         _, htext = availtext.get_size()
 
         bar_height = max(total_height - htext, 1) # don't go lower than one pixel
 
         rect = pygame.Rect((xrect, yrect + htext), (rect_width, bar_height))
         if current_availability > 0:
-            _ = pygame.draw.rect(self.window, self.colors['green'], rect)
+            _ = pygame.draw.rect(self.surface, self.bar_color_positive, rect, border_radius = self.rectangle_radius)
         else:
-            _ = pygame.draw.rect(self.window, self.colors['red'], rect)
+            _ = pygame.draw.rect(self.surface, self.bar_color_negative, rect, border_radius = self.rectangle_radius)
+
+    def display_run_info(self):
+        runinfo = self.runinfofont.render('run {} out of {}'.format(self.run_index, self.max_runs),
+                                     True,
+                                     self.colors['black'])
+        wtext, htext = runinfo.get_size()
+        self.surface.blit(runinfo, (self.window_size[0] - wtext - self.margin, self.margin))
+
 
 
 if __name__ == "__main__":
@@ -212,7 +266,7 @@ if __name__ == "__main__":
     # initialize simulation
     ms = Simulation(config, runs=1, overwrite_print=False)
     ms.run()
-    sa = simAnimation(ms, timestep_sim_seconds=15)
+    sa = simAnimation(ms, timestep_sim_seconds=60, fps=24)
     sa.run()
 
 
